8d1cb8a419eba668ab4b4eae4f3f6784fb893b17
Maria Ivz <113674087+okuenz@users.noreply.github.com>
2023-11-20 21:57:18 +0300

    GemPicker and Health

---
+++ Assets/Scripts/CollisionDamage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDamage : MonoBehaviour
+{
+    public int collisionDamage = 1;
+    public string collisionTag;
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == collisionTag)
+        { 
+            Health health = coll.gameObject.GetComponent<Health>();
+            health.TakeHit(collisionDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int maxHealth;
+
+    public void TakeHit(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        { 
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetHealth(int bonusHealth)
+    {
+        health += bonusHealth;
+
+        if (health > maxHealth)
+        { 
+            health = maxHealth;
+        }
+
+    }
+
+}
