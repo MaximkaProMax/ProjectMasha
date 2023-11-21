6fa374565436c40b6abab120d491a38b75ee326f
Maria Ivz <ivzmaria9@gmail.com>
2023-11-21 13:58:31 +0300

    GemPicker & Health
    счетчик кристаллов и отображение системы здоровья

--- Assets/Scripts/GemPicker.cs
+++ Assets/Scripts/GemPicker.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GemPicker : MonoBehaviour
 {
+    public TMP_Text coinsText;
+    private float coins = 0;
     private void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Gem")
-        { 
+        {
+            coins++;
+            coinsText.text = coins.ToString();
             Destroy(coll.gameObject);
         }
     }
