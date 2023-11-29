c924c06d88ba03c341d414a631c054e34926e6f4
Maxim <Zholdashev.Maks@yandex.ru>
2023-11-29 15:37:55 +0300

    Fix Enemy 1.0

--- Assets/Scripts/PlatformPatrol.cs
+++ Assets/Scripts/PlatformPatrol.cs
@@ -9,34 +9,38 @@ public class PlatformPatrol : MonoBehaviour
 
     private bool movingRight = true;
     public Transform groundDetection;
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        // Определяем направление движения
+        Vector3 direction = movingRight ? Vector3.right : Vector3.left;
+        transform.Translate(direction * speed * Time.deltaTime);
 
+        // Создаем обнаружение земли впереди
         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
 
+        // Проверяем столкновение с препятствием
         if (groundInfo.collider == false)
         {
-            if (movingRight == true)
-            { 
-                transform.eulerAngles = new Vector3(0, 180, 0);
-                movingRight = false;
-            }
-
-            else
-            {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
-            }
+            // Поворачиваем, если нет земли впереди
+            Flip();
         }
 
+        // Проверяем столкновение с препятствием слева или справа
+        RaycastHit2D obstacleInfo = Physics2D.Raycast(transform.position, direction, 1f);
+        if (obstacleInfo.collider != null)
+        {
+            // Поворачиваем, если есть препятствие слева или справа
+            Flip();
+        }
+    }
 
-
-        
+    void Flip()
+    {
+        // Меняем направление движения и разворачиваем объект
+        movingRight = !movingRight;
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
     }
 }
