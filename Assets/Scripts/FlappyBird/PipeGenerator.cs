5e42c502528b2b71a32e8b78afff9782bca67c7c
Maxim <Zholdashev.Maks@yandex.ru>
2023-12-01 01:09:01 +0300

    FlappyBird 3.0

--- Assets/Scripts/FlappyBird/PipeGenerator.cs
+++ Assets/Scripts/FlappyBird/PipeGenerator.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,56 +8,52 @@ public class PipeGenerator : MonoBehaviour
     public float spawnInterval = 2f;
     public float minY = -5f;  // Минимальная координата Y
     public float maxY = 5f;   // Максимальная координата Y
-    public float spawnRangeX = 10f; // Расстояние вдоль оси X, на котором будут появляться трубы
+    public float spawnRangeX = 10f; // Расстояние вдоль оси X от игрока, на котором будут появляться трубы
 
-    private bool spawningFromTop = true; // флаг, указывающий, сверху или снизу появится следующая труба
+    private Transform playerTransform; // Ссылка на трансформ игрока
     private List<GameObject> pipes = new List<GameObject>(); // Список всех созданных труб
+
+    void Start()
+    {
+        // Находим трансформ игрока
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     void Update()
     {
-        // Запускаем метод создания труб
-        SpawnPipe();
-
         // Удаляем трубы, которые ушли за пределы игровой области
-        for (int i = 0; i < pipes.Count; i++)
+        for (int i = pipes.Count - 1; i >= 0; i--)
         {
-            if (pipes[i].transform.position.x < transform.position.x - spawnRangeX)
+            if (pipes[i].transform.position.x < playerTransform.position.x - spawnRangeX)
             {
                 Destroy(pipes[i]);
                 pipes.RemoveAt(i);
-                i--;
             }
+        }
+
+        // Запускаем метод создания труб только если прошло достаточно времени
+        if (Time.time >= spawnInterval)
+        {
+            SpawnPipe();
+            spawnInterval += 2f; // Увеличиваем интервал по времени для следующего создания трубы
         }
     }
 
     void SpawnPipe()
     {
-        // Проверяем, нужно ли создать новую трубу в данный момент
-        if (Time.time >= spawnInterval)
-        {
-            // Сбрасываем таймер
-            spawnInterval = Time.time + 2f;
+        // Выбираем случайный префаб из массива
+        GameObject selectedPipePrefab = pipePrefabs[Random.Range(0, pipePrefabs.Length)];
 
-            // Выбираем случайный префаб из массива
-            GameObject selectedPipePrefab = pipePrefabs[Random.Range(0, pipePrefabs.Length)];
+        // Создаем новую трубу основываясь на выбранном префабе
+        GameObject newPipe = Instantiate(selectedPipePrefab);
 
-            // Создаем новую трубу основываясь на выбранном префабе
-            GameObject newPipe = Instantiate(selectedPipePrefab);
+        // Определяем позицию Y для появления новой трубы
+        float spawnY = Random.Range(minY, maxY);
 
-            // Определяем позицию для появления новой трубы
-            float spawnY = spawningFromTop ? maxY : minY;
+        // Позиционируем трубу в указанной позиции по оси Y и X на расстоянии spawnRangeX от игрока
+        newPipe.transform.position = new Vector2(playerTransform.position.x + spawnRangeX, spawnY);
 
-            // Позиционируем трубу в указанной позиции по оси Y и X
-            newPipe.transform.position = new Vector2(transform.position.x + spawnRangeX, spawnY);
-
-            // Добавляем коллайдер к новой трубе
-            BoxCollider2D pipeCollider = newPipe.AddComponent<BoxCollider2D>(); // Вот здесь исправлено!
-
-            // Добавляем новую трубу в список
-            pipes.Add(newPipe);
-
-            // Инвертируем флаг для следующей трубы
-            spawningFromTop = !spawningFromTop;
-        }
+        // Добавляем новую трубу в список
+        pipes.Add(newPipe);
     }
 }
